1e565d86966438a4df2c0d791ba7a2e91d61fcdc
PIH <zasxcc@naver.com>
2020-07-27 21:13:31 +0900

    keyItem 스킨 변경, 회전 추가

--- Source/ReadiedBullet/Private/KeyItem.cpp
+++ Source/ReadiedBullet/Private/KeyItem.cpp
@@ -11,7 +11,7 @@ AKeyItem::AKeyItem()
 	CapCom = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleColl"));
 	CapCom->SetupAttachment(StaticMesh);
 	CapCom->ComponentTags.Add(FName("Key"));
-
+	PrimaryActorTick.bCanEverTick = true;
 }
 
 // Called when the game starts or when spawned
@@ -19,13 +19,14 @@ void AKeyItem::BeginPlay()
 {
 	Super::BeginPlay();
 	CapCom->OnComponentBeginOverlap.AddDynamic(this, &AKeyItem::BeginOverlap);
+	
 }
 
 // Called every frame
 void AKeyItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	CapCom->AddLocalRotation(FRotator(0.0f, 5.0f, 0.0f));
 }
 
 void AKeyItem::BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
