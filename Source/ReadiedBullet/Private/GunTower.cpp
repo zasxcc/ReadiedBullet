a98798924e8750c1695450bcb24f740b2f36fc2e
PIH <zasxcc@naver.com>
2020-08-17 13:57:33 +0900

    BlackHole, GunTower 추가

---
+++ Source/ReadiedBullet/Private/GunTower.cpp
@@ -0,0 +1,121 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GunTower.h"
+
+// Sets default values
+AGunTower::AGunTower()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SKMesh"));
+	HPBarWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("HPBarWidget"));
+	AudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("Audio_Fire"));
+
+	AudioComponent->bAutoActivate = false;
+	AudioComponent->SetupAttachment(Mesh);
+
+	static ConstructorHelpers::FObjectFinder<USoundBase>FIRESOUND(TEXT("SoundWave'/Game/Sound/Tank_Fire.Tank_Fire'"));
+	if (FIRESOUND.Succeeded())
+	{
+		FireCue = FIRESOUND.Object;
+	}
+
+	static ConstructorHelpers::FClassFinder<UUserWidget> UI_HUD(TEXT("/Game/UI/WBP_HPBar"));
+	if (UI_HUD.Succeeded())
+	{
+		HPBarWidget->SetWidgetClass(UI_HUD.Class);
+		HPBarWidget->SetDrawSize(FVector2D(200.0f, 80.0));
+	}
+	HPBarWidget->SetRelativeLocation(FVector(0.0f, 0.0f, 200.0f));
+	HPBarWidget->SetWidgetSpace(EWidgetSpace::Screen);
+
+	static ConstructorHelpers::FObjectFinder<UAnimSequence> FireAnim(TEXT("AnimSequence'/Game/CSC/Animations/Anim_CSC_Gun4.Anim_CSC_Gun4'"));
+	S_FireAnim = FireAnim.Object;
+
+	MaxHP = 1.0f;
+	isFire = false;
+	fireTime = 0.0f;
+}
+
+// Called when the game starts or when spawned
+void AGunTower::BeginPlay()
+{
+	Super::BeginPlay();
+	HPBarWidget->SetVisibility(false);
+}
+
+// Called every frame
+void AGunTower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	FVector Loc = GetActorLocation();
+	FVector PlayerLoc = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)->GetActorLocation();
+	float dis = UKismetMathLibrary::Vector_Distance(PlayerLoc, Loc);
+	if (dis < 3500)
+	{
+		isFire = true;
+		fireTime += DeltaTime;
+	}
+	else
+	{
+		isFire = false;
+		fireTime = 0.0f;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("%d"), dis);
+	if (isFire)
+	{
+		FVector playerLoc = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)->GetActorLocation();
+		FRotator rot = UKismetMathLibrary::FindLookAtRotation(Mesh->GetComponentLocation(), playerLoc);
+		Mesh->SetWorldRotation(rot);
+	}
+
+	if (isFire && fireTime > 2.5f) {
+		Mesh->PlayAnimation(S_FireAnim, false);
+		Fire();
+		HPBarWidget->SetVisibility(true);
+		fireTime = 0.0f;
+	}
+}
+
+void AGunTower::Fire()
+{
+
+	AudioComponent->AttenuationSettings;
+	AudioComponent->SetSound(FireCue);
+	AudioComponent->Play(1.0f);
+
+	FVector EyeLocation;
+	FRotator EyeRotation;
+
+	this->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
+
+	FVector ShotDirection = EyeRotation.Vector();
+
+	// Bullet Spread
+	float HalfRad = FMath::DegreesToRadians(BulletSpread);
+	ShotDirection = FMath::VRandCone(ShotDirection, HalfRad, HalfRad);
+
+	FVector TraceEnd = EyeLocation + (ShotDirection * 10000);
+	if (ProjectileClass)
+	{
+
+		//무기 위치 받아서 저장
+		FVector MuzzleLocation = Mesh->GetSocketLocation("FireSocket");
+		//FRotator MuzzleRotation = MeshComp->GetSocketRotation("MuzzleFlashSocket");;
+
+		//Set Spawn Collision Handling Override
+		FActorSpawnParameters ActorSpawnParams;
+		ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+
+		FVector meshLoc = Mesh->GetComponentLocation();
+
+		// spawn the projectile at the muzzle
+		GetWorld()->SpawnActor<AActor>(ProjectileClass, meshLoc, Mesh->GetComponentRotation(), ActorSpawnParams);
+	}
+
+	//LastFireTime = GetWorld()->TimeSeconds;
+
+}
---
+++ Source/ReadiedBullet/Public/GunTower.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "MonsterWidget.h"
+#include "Components/WidgetComponent.h"
+#include "GameFramework/Actor.h"
+#include "Components/AudioComponent.h"
+#include "Sound/SoundBase.h"
+#include "Sound/SoundAttenuation.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/SceneComponent.h"
+#include "GunTower.generated.h"
+
+UCLASS()
+class READIEDBULLET_API AGunTower : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGunTower();
+	void Fire();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Bodt)
+	USkeletalMeshComponent* Mesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Projectile")
+	TSubclassOf<AActor> ProjectileClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon", meta = (ClampMin = 0.0f))
+	float BulletSpread;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Sound")
+	USoundBase* FireCue;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Sound")
+	class UAudioComponent* AudioComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "UI")
+	UWidgetComponent* HPBarWidget;
+
+	float MaxHP;
+	bool isFire;
+	float fireTime;
+	UAnimSequence* S_FireAnim;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
